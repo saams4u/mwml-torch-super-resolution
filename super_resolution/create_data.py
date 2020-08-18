5a70246eef1035309ae38c7b66378aaa687262dc
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2020-08-18 18:53:12 -0500

    new updates

--- super_resolution/create_data.py
+++ super_resolution/create_data.py
@@ -4,8 +4,6 @@ from utils import create_data
 
 if __name__ == '__main__':
 	create_data(train_folders=['../input_data/train2014', '../input_data/val2014'],
-				test_folders=['../input_data/BSDS100_SR/image_SRF_4', 
-							  '../input_data/Set5_SR/Set5/image_SRF_4', 
-							  '../input_data/Set14_SR/Set14/image_SRF_4'],
+				test_folders=['../input_data/BSDS100_SR/image_SRF_4', '../input_data/Set5_SR/Set5/image_SRF_4', '../input_data/Set14_SR/Set14/image_SRF_4'],
 				min_size=100,
 				output_folder='../output_lists')
